a4bf2265b1de5312cdaebabb91f80e0cb61e1b66
Polina Limonova <polina98_08@mail.ru>
2024-04-11 17:57:06 +0300

    Adaptive version created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
--- frontend/src/components/AuthLayout.tsx
+++ frontend/src/components/AuthLayout.tsx
@@ -4,9 +4,13 @@ import React from "react";
 
 const AuthLayout = ({ image, children }) => {
   return (
-    <div className="flex">
-      <img src={image} alt="photo" className="w-[50vw] h-[100vh]" />
-      <div className="w-[50vw] h-[100vh] flex flex-col items-center justify-center">
+    <div className="flex flex-col gap-[50px] lg:flex-row">
+      <img
+        src={image}
+        alt="photo"
+        className="w-screen h-[300px] md:h-[500px] object-cover  lg:object-fill lg:w-[50vw] lg:h-[100vh]"
+      />
+      <div className="w-screen lg:w-[50vw] lg:h-[100vh] flex flex-col items-center justify-center">
         <div className="w-[50%] text-center">{children}</div>
       </div>
     </div>
