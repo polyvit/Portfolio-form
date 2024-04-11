2acb76b16c2ea608c018fc2b7da6c968c877ac84
Polina Limonova <polina98_08@mail.ru>
2024-04-11 12:57:23 +0300

    Add logout button to form page

--- frontend/src/pages/FormPage.tsx
+++ frontend/src/pages/FormPage.tsx
@@ -2,18 +2,23 @@ import React from "react";
 
 const FormPage = () => {
   return (
-    <div>
-      <h1>Здесь будет форма по добавлению новых проектов. </h1>
-      <h2 className="my-10">
-        Если вы не видите кнопку, значит вы просто посетитель и не имеете право
-        на добавление проектов
-      </h2>
-      <button
-        type="button"
-        className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
-      >
-        Добавить
+    <div className="relative w-screen h-screen flex items-center justify-center">
+      <button className="absolute top-5 right-5 text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+        Logout
       </button>
+      <div className="text-center">
+        <h1>Здесь будет форма по добавлению новых проектов. </h1>
+        <h2 className="my-10">
+          Если вы не видите кнопку, значит вы просто посетитель и не имеете
+          право на добавление проектов
+        </h2>
+        <button
+          type="button"
+          className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+        >
+          Добавить
+        </button>
+      </div>
     </div>
   );
 };
