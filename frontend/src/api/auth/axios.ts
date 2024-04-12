026628edea19443295f5b3f341ae71e21255ad30
Polina Limonova <polina98_08@mail.ru>
2024-04-12 16:42:59 +0300

    Axios request interceptor

--- frontend/src/api/auth/axios.ts
+++ frontend/src/api/auth/axios.ts
@@ -1,4 +1,7 @@
 import axios, { CreateAxiosDefaults } from "axios"
+import AuthHelper from "./auth.helper"
+import ApiHelper from "../api.helper"
+import AuthService from "./auth.service"
 
 const AUTH_API_URL = "http://localhost:5000/auth"
 
@@ -11,3 +14,26 @@ const axiosOptions: CreateAxiosDefaults = {
 }
 
 export const axiosInstance = axios.create(axiosOptions)
+const axiosFormInstance = axios.create(axiosOptions)
+
+axiosFormInstance.interceptors.request.use(config => {
+    const accessToken = AuthHelper.getAccessToken()
+    if (config.headers && accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`
+    }
+    return config
+})
+
+axiosFormInstance.interceptors.response.use(config => config, async error => {
+    console.log("interceptor", error)
+    // const request = error.config
+
+    // if ((error.response.status === 401 || ApiHelper.getErrorMessage(error) === "" || ApiHelper.getErrorMessage(error) === "") && error.config && !error.config._isRetry) {
+    //     request._isRetry = true
+    //     try {
+            
+    //     } catch (e) {}
+    // }
+
+    throw error
+})
