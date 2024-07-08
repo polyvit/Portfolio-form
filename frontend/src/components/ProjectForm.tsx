7e4307d1b0a21d92a45219ab329ec66bfad9e8f2
Polina Limonova <polina98_08@mail.ru>
2024-07-08 11:06:37 +0300

    Stepper refactoring with a hook

--- frontend/src/components/ProjectForm.tsx
+++ frontend/src/components/ProjectForm.tsx
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
-import Textarea from "../elements/Textarea";
 import Stepper from "./Stepper";
 import GeneralData from "./GeneralData";
 import ImageDropdown from "./ImageDropdown";
 import Stack from "./Stack";
-import useInput from "../hooks/use-input";
+import Tasks from "./Tasks";
+import useStepper from "../hooks/use-stepper";
 
 const steps = ["Основное", "Картинки", "Стек", "Дополнительно"];
 const stack = [
@@ -21,14 +20,26 @@ const stack = [
 ];
 
 const ProjectForm = () => {
-  const [stepNumber, setStepNumber] = useState(0);
-  const tasks = useInput("", { isEmpty: true });
+  const {
+    stepIndex,
+    step,
+    next,
+    back,
+    setStepNumber,
+    isFirstStep,
+    isLastStep,
+  } = useStepper([
+    <GeneralData />,
+    <ImageDropdown />,
+    <Stack stack={stack} />,
+    <Tasks />,
+  ]);
 
   const handleMainBtnClick = () => {
-    if (stepNumber === steps.length - 1) {
+    if (isLastStep) {
       console.log("Отправлено");
     } else {
-      setStepNumber((stepNumber) => stepNumber + 1);
+      next();
     }
   };
 
@@ -36,31 +47,14 @@ const ProjectForm = () => {
     <div className="flex flex-col items-center gap-4">
       <Stepper
         steps={steps}
-        stepNumber={stepNumber}
+        stepNumber={stepIndex}
         setStepNumber={setStepNumber}
       />
-      <form className="w-[500px] grow">
-        {stepNumber === 0 && <GeneralData />}
-        {stepNumber === 1 && <ImageDropdown />}
-        {stepNumber === 2 && <Stack stack={stack} />}
-        {stepNumber === 3 && (
-          <Textarea
-            label="Задачи на доработку"
-            placeholder="Перечислите через запятую, какие задачи по проекту вы планируете выполнить в дальнейшем"
-            value={tasks.value}
-            onChange={tasks.onChange}
-            onBlur={tasks.onBlur}
-            error={tasks.errorText}
-            touched={tasks.wasTouched}
-            invalid={tasks.inputValid}
-            rows={8}
-          />
-        )}
-      </form>
+      <form className="w-[500px] grow">{step}</form>
       <div className="flex gap-4">
-        {stepNumber !== 0 && (
+        {!isFirstStep && (
           <button
-            onClick={() => setStepNumber((stepNumber) => stepNumber - 1)}
+            onClick={back}
             type="button"
             className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
           >
@@ -72,7 +66,7 @@ const ProjectForm = () => {
           type="button"
           className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
         >
-          {stepNumber === steps.length - 1 ? "Отправить" : "Далее"}
+          {isLastStep ? "Отправить" : "Далее"}
         </button>
       </div>
     </div>
